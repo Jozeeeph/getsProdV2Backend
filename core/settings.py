7dbf1ec6edfcba08344f44d4ee6c4467aaedb057
Jozeeeph <yousseef.bs123@gmail.com>
2024-12-24 23:50:12 +0100

    backend works clearly

--- core/settings.py
+++ core/settings.py
@@ -39,18 +39,30 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
+
+
+
+CORS_ORIGIN_ALLOW_ALL = True # Allow all origins (adjust for production)
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
--- products/models.py
+++ products/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-from django.core.validators import MinValueValidator
 
-
-# Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    price = models.FloatField(validators=[MinValueValidator(0.0)])
-    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
